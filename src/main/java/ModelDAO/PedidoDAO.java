86bb563d45a50470fa43c3e077e0a3c2255d6b4d
DeusEli <adonayvm32@gmail.com>
2023-03-01 17:33:01 -0600

    PedidoDAO && Connect modified

--- src/main/java/ModelDAO/PedidoDAO.java
+++ src/main/java/ModelDAO/PedidoDAO.java
@@ -18,7 +18,7 @@ public class PedidoDAO implements IPedidoCRUD {
     PreparedStatement ps;
     ResultSet rs;
 
-    private List getPedidoList(List<Pedido> list, String sql) {
+    private List getPedidoList(List<Pedido> list, String sql)    {
         try {
             conn = cn.getConnection();
             ps = conn.prepareStatement(sql);
@@ -26,9 +26,10 @@ public class PedidoDAO implements IPedidoCRUD {
             while (rs.next()) {
                 Pedido pedido = new Pedido();
                 pedido.setId_pedido(rs.getInt("id_pedido"));
-                pedido.setId_cliente(rs.getInt("id_usuario"));
+                pedido.setId_cliente(rs.getInt("id_cliente"));
                 pedido.setFecha(rs.getDate("fecha"));
                 pedido.setTotal(rs.getDouble("total"));
+                pedido.setEstado(rs.getString("estado"));
                 list.add(pedido);
             }
         } catch (Exception e) {
@@ -40,20 +41,20 @@ public class PedidoDAO implements IPedidoCRUD {
     @Override
     public List getPedidos() {
         List<Pedido> list = new ArrayList<>();
-        String sql = "SELECT * FROM pedido";
+        String sql = "SELECT * FROM Pedido";
         return getPedidoList(list, sql);
     }
 
     @Override
     public List getPedido(int id_pedido) {
         List<Pedido> list = new ArrayList<>();
-        String sql = "SELECT * FROM pedido WHERE id_pedido = '" + id_pedido + "'";
+        String sql = "SELECT * FROM Pedido WHERE id_pedido = '" + id_pedido + "'";
         return getPedidoList(list, sql);
     }
 
     @Override
     public boolean addPedido(Pedido pedido) throws ClassNotFoundException, SQLException {
-        String sql = "INSERT INTO pedido (id_usuario, fecha, total) VALUES ('" + pedido.getId_cliente() + "', '" + pedido.getFecha() + "', '" + pedido.getTotal() + "')";
+        String sql = "INSERT INTO Pedido (id_cliente, fecha, total, estado) VALUES ('" + pedido.getId_cliente() + "', '" + pedido.getFecha() + "', '" + pedido.getTotal() + "', '" + pedido.getEstado() + "')";
         conn = cn.getConnection();
         ps = conn.prepareStatement(sql);
         ps.executeUpdate();
@@ -62,7 +63,7 @@ public class PedidoDAO implements IPedidoCRUD {
 
     @Override
     public boolean updatePedido(Pedido pedido) throws ClassNotFoundException, SQLException{
-        String sql = "UPDATE pedido SET id_usuario = '" + pedido.getId_cliente() + "', fecha = '" + pedido.getFecha() + "', total = '" + pedido.getTotal() + "' WHERE id_pedido = '" + pedido.getId_pedido() + "'";
+        String sql = "UPDATE Pedido SET id_cliente = '" + pedido.getId_cliente() + "', fecha = '" + pedido.getFecha() + "', total = '" + pedido.getTotal() + "', estado = '" + pedido.getEstado() + "' WHERE id_pedido = '" + pedido.getId_pedido() + "'";
         conn = cn.getConnection();
         ps = conn.prepareStatement(sql);
         ps.executeUpdate();
@@ -70,7 +71,7 @@ public class PedidoDAO implements IPedidoCRUD {
     }
     @Override
     public boolean deletePedido(int id_pedido) throws ClassNotFoundException, SQLException {
-        String sql = "DELETE FROM pedido WHERE id_pedido = '" + id_pedido + "'";
+        String sql = "DELETE FROM Pedido WHERE id_pedido = '" + id_pedido + "'";
         conn = cn.getConnection();
         ps = conn.prepareStatement(sql);
         ps.executeUpdate();
